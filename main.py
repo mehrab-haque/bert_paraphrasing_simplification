0596c4ae976593f42bbdd48d49388e400ce6ae96
mehrab-haque <mehrab.haque.0001@gmail.com>
2021-01-27 18:16:04 +0600

    added difficult word dynamic filtration

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+from re import split
+
 from transformers import pipeline
 import textstat
+from nltk.corpus import words
 
 nlp = pipeline("fill-mask")
-preds = nlp(f"I am excited, it's a {nlp.tokenizer.mask_token} topic.")
 
-new_sentences=[]
-ease_levels=[]
+sentence = input('Enter a sentence with difficult words :')
+wordList = split('[^a-zA-Z]', sentence)
+for w in wordList:
+     if w in words.words() and int(textstat.difficult_words(w)) == 1:  # If the word is difficult
+         preds = nlp(sentence.replace(w,'<mask>'))
+         new_sentences = []
+         ease_levels = []
+         for p in preds:
+             s = sentence.replace(w,str(nlp.tokenizer.decode([p['token']])).strip())
+             new_sentences.append(s)
+             ease_levels.append(float(textstat.flesch_reading_ease(s)))
+         sentence = new_sentences[ease_levels.index(min(ease_levels))]
 
-for p in preds:
-    s = "I am excited, it's a " + str(nlp.tokenizer.decode([p['token']])).strip() + " topic."
-    new_sentences.append(s)
-    ease_levels.append(float(textstat.flesch_reading_ease(s)))
+print(sentence)
 
-print("original sentence : I am excited, it's a **** topic.")
-print("simplified sentence : "+new_sentences[ease_levels.index(min(ease_levels))])
+
 
 
